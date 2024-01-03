0c7fa043babf3495e6713a4056929f25f9f11e99
mallikharjun999 <135102124+mallikharjun999@users.noreply.github.com>
2024-01-03 12:52:03 +0530

    Update main.tf

--- devops/terrafrom/terraform_s3_Bucket/main.tf
+++ devops/terrafrom/terraform_s3_Bucket/main.tf
@@ -2,8 +2,8 @@
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAVSZIXFAE7ZIGUKXS"
-  secret_key = "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"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
 
 
